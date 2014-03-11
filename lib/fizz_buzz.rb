d782e9ff3397a6365cba9dad0bbebe8feaf03201
Bebe Peng <bebepeng@gmail.com>
2014-03-11 14:06:21 -0600

    Array with Fizz in place of numers that are multiples of 3

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -5,5 +5,14 @@ class FizzBuzz
 
   def print_array
     nums = 1.upto(@num).to_a
+    i = 0
+    nums.each do |n|
+      case
+        when n % 3 == 0
+          nums[i]="Fizz"
+      end
+      i+=1
+    end
+    nums
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -3,11 +3,17 @@ require 'fizz_buzz'
 describe FizzBuzz do
 
   it "passes an array of numbers starting at 1 and ending at the input" do
-    fizzbuzz = FizzBuzz.new(10)
-    expected = [1 ,2, 3, 4, 5, 6, 7, 8 , 9, 10]
+    fizzbuzz = FizzBuzz.new(2)
+    expected = [1 ,2]
     expect(fizzbuzz.print_array).to eq expected
   end
-  it "replaces all multiples of 3 with Fizz"
+
+  it "replaces all multiples of 3 with Fizz" do
+    fizzbuzz = FizzBuzz.new(10)
+    expected = [1 ,2, "Fizz", 4, 5, "Fizz", 7, 8 ,"Fizz", 10]
+    expect(fizzbuzz.print_array).to eq expected
+  end
+
   it "replaces all multiples of 5 with Buzz"
   it "replaces all multiples of both 3 and 5 with FizzBuzz"
 end
