9222fe90c0ccb081eae85eb5edeb007d7f46a8b7
Bebe Peng <bebepeng@gmail.com>
2014-03-16 16:52:40 -0600

    Refactored test instantiation
    refactored for less repetition

--- spec/leap_year_calculator_spec.rb
+++ spec/leap_year_calculator_spec.rb
@@ -1,13 +1,14 @@
 require 'leap_year_calculator'
 
 describe LeapYearCalculator do
+  before do
+    @calc = LeapYearCalculator.new
+  end
   it "tells me if it's a leap year if it's divisible by 4" do
-    calc = LeapYearCalculator.new
-    expect(calc.yes?(1996)).to eq true
+    expect(@calc.yes?(1996)).to eq true
   end
   it "correctly tells me it's not a leap year" do
-    calc = LeapYearCalculator.new
-    expect(calc.yes?(2001)).to eq false
+    expect(@calc.yes?(2001)).to eq false
   end
   it "tells me it's not a leap year if it's only divisible by 100"
 end
